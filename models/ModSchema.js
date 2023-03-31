08bc7ecaeb539cef858ef90228007b1000d3a559
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2023-03-31 23:40:34 +0100

    Working with mongoDB and creating an ban command.
    Using mongodb to store the punishment data of the user in that guild, stores guildid, userid and an punishment array which contains the punishment type, the responsible moderator, the reason, and the randomly generated punishment id.

---
+++ models/ModSchema.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+let ModSchema = new mongoose.Schema({
+    GuildID: String,
+    UserID: String,
+    Punishments: Array,
+});
+module.exports = mongoose.model("Moderation", ModSchema);
