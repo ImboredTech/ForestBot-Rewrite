5737970081db0508506e5a330a44a337c7bcc815
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2023-04-02 15:48:22 +0100

    Create userinfo.js
    Created the userinfo command you can specify an user or not and it will get the information of you or the specified user.

---
+++ commands/information/userinfo.js
@@ -0,0 +1,42 @@
+const { ApplicationCommandOptionType, Client, Interaction, EmbedBuilder} = require('discord.js');
+
+module.exports =
+ {
+    /**
+     * 
+     * @param {Client} client
+     * @param {Interaction} interaction
+     */
+    
+    callback: async(client, interaction) => {
+        const mentionedUserId = interaction.options.get("target-user")?.value;
+        const targetUserId = mentionedUserId || interaction.member.id;
+        const targetUserObj = await interaction.guild.members.fetch(targetUserId);
+        await interaction.deferReply();
+
+        const accountCreationDate = targetUserObj.createdAt.toLocaleDateString('en-GB', { dateStyle: 'medium' });
+        const guildJoinDate = interaction.guild.members.cache.get(targetUserId).joinedAt.toLocaleDateString('en-GB', { dateStyle: 'medium' });
+        const userName = targetUserObj.username;
+        const discriminator = targetUserObj.discriminator;
+        const avatar = targetUserObj.displayAvatarURL({ format: 'png', dynamic: true, size: 4096 });
+        const highestRole = interaction.guild.members.cache.get(targetUserId).roles.highest;
+        
+        const embed = new EmbedBuilder()
+            .setTitle(`User Info: ${userName}#${discriminator}`)
+            .setColor('Green')
+            .setThumbnail(avatar)
+            .addFields({ name: "Account Creation Date", value: `${accountCreationDate}`})
+            .addFields({ name: "Guild Join Date", value: `${guildJoinDate}` })
+            .addFields({ name: "Highest Role", value: `${highestRole}`});
+
+        await interaction.editReply({ embeds: [embed] });
+    },  
+    name: 'userinfo',
+    description: "Gets user information of you or the specified user.",
+    options: [{
+        name: 'target-user',
+        description: "The user you want to see the information of.",
+        type: ApplicationCommandOptionType.User,
+        required: false,
+    }]
+};
