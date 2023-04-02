3ade49725bf41466b109b9db0cb3ee38bc520250
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2023-04-02 11:06:25 +0100

    Update autoMod.js
    Trying some trouble shooting to see if i can find out why the auto mod feature is not working

--- events/messageCreate/autoMod.js
+++ events/messageCreate/autoMod.js
@@ -7,27 +7,23 @@ const AutoMod = require("../../models/AutoMod");
  * @param {Message} message
  */
 module.exports = async (client, message) => {
-    const guildId = message.guild.id;
+  const guildId = message.guild.id;
 
-    const data = await AutoMod.findOne({ guildId: guildId});
+  const data = await AutoMod.findOne({ guildId: guildId });
 
-    if(!data || !data.autoModEnabled) return;
+  if (!data || !data.autoModEnabled) return;
 
-    if(data || data.autoModEnabled) {
-        const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/gi;
-        const discordRegex = /(discord\.gg\/|discordapp\.com\/invite\/)/gi;
-        const twitchRegex = /(?:https?:\/\/)?(?:www\.)?(?:twitch\.tv)\/(.+)/gi;
-        const racialSlursRegex = /(?:n[\*\^]{1,}g{1,}a{0,1}|b[\*\^]{1,}tch|f[\*\^]{1,}g{1,}|s[\*\^]{1,}t)/gi;
-        const messageContent = message.content.toLowerCase();
-    
-        if(youtubeRegex.test(messageContent) || discordRegex.test(messageContent) || twitchRegex.test(messageContent)) {
-            await message.delete();
-    
-           await message.channel.send(`${message.author}, you can't sned links here!`);
-        } else if(racialSlursRegex.test(messageContent)) {
-            await message.delete();
-    
-           await message.channel.send(`${message.author}, you can't say that word here.`);
-        }
-    }
-};
+  const youtubeRegex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([-\w]{11})$/gi;
+  const discordRegex = /(discord\.gg\/|discordapp\.com\/invite\/)/gi;
+  const twitchRegex = /^(?:https?:\/\/)?(?:www\.)?(?:twitch\.tv)\/([a-zA-Z0-9_]{4,25})$/gi;
+  const racialSlursRegex = /\bn[i1]gg[aeu]?r\b|\bb[i1]tch\b|\bf[aeu]gg[aeu]t\b|\bs[i1]t\b/gi;
+  const messageContent = message.content.toLowerCase();
+
+  if (youtubeRegex.test(messageContent) || discordRegex.test(messageContent) || twitchRegex.test(messageContent)) {
+    await message.delete();
+    await message.channel.send(`${message.author}, you can't send links here!`);
+  } else if (racialSlursRegex.test(messageContent)) {
+    await message.delete();
+    await message.channel.send(`${message.author}, you can't say that word here.`);
+  }
+}
