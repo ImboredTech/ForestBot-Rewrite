2d7e9b2b3ebf1651f978ef2939c1329f3e0053a3
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2023-04-02 14:43:14 +0100

    Update automod-enable
    Fixing some grammar error that I made

--- commands/admin/automod-enable.js
+++ commands/admin/automod-enable.js
@@ -25,7 +25,7 @@ module.exports = {
                 automodEnabled: true,
             });
             data.save();
-            await interaction.editReply("I have enabled auto moderator for this server. To disable it run the /automod-disable command.");
+            await interaction.editReply("I have enabled auto moderation for this server. To disable it run the /automod-disable command.");
         } else if(data || data.automodEnabled) {
             await interaction.editReply("Automod has already been enabled. If you are trying to disable automod do /automod-disable to disable the automod.");
         }
