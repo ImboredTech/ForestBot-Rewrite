d61dee3521d1374fc0ab3fd812c75b76b10f0d72
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2023-04-04 18:59:54 +0100

    Update bot.js
    Starting to uncomment the code for the dashboard :+1:

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const { REST, Routes, Client, IntentsBitField, Collection} = require("discord.js");
 const mongoose = require("mongoose");
-//const express = require("express");
-//const app = express();
+const express = require("express");
+const app = express();
 const os = require('os');
 const eventHandler = require("./handlers/eventHandler");
 const fs = require("fs");
@@ -16,59 +16,48 @@ const client = new Client({
 });
 
 client.commands = new Collection();
-// THE FOLLOWING COMMENTED IMPORTS ARE FOR THE FUTURE DASHBOARD.
-// const cookieParser = require("cookie-parser");
-// const urlencodedParser = require("body-parser").urlencoded({ extended: false });
-// const DiscordOauth2 = require('discord-oauth2');
+const cookieParser = require("cookie-parser");
+const urlencodedParser = require("body-parser").urlencoded({ extended: false });
+const DiscordOauth2 = require('discord-oauth2');
 
 module.exports.client = client;
-// app.enable("trust proxy");
-// app.set("etag", false);
-// app.use(express.static(__dirname + "/dashboard"));
-// app.set("views", __dirname);
-// app.set("view engine", "ejs");
-// app.use(cookieParser());
-// app.use(urlencodedParser);
-// process.oauth = new DiscordOauth2({
-    //clientId: process.env.CLIENT_ID,
-    //clientSecret: process.env.CLIENT_SECRET,
-    //redirectUri: process.env.REDIRECT_URI,
-//});
+app.enable("trust proxy");
+app.set("etag", false);
+app.use(express.static(__dirname + "/dashboard"));
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+app.use(cookieParser());
+app.use(urlencodedParser);
+process.oauth = new DiscordOauth2({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    redirectUri: process.env.REDIRECT_URI,
+});
 
-//app.use((req, res, next) => {
-    //console.log(`- ${req.method}: ${req.url} ${res.statusCode} ( by: ${req.ip} )`);
-    //next();
-//});
+app.use((req, res, next) => {
+    console.log(`- ${req.method}: ${req.url} ${res.statusCode} ( by: ${req.ip} )`);
+    next();
+});
 
-// Request Handler
+let files = fs.readdirSync("./dashboard/public").filter(f => f.endsWith(".js"));
 
-// Get all the files in the public folder that ends with .js
-// let files = fs.readdirSync("./dashboard/public").filter(f => f.endsWith(".js"));
-
-// Looping through all files in it
-// files.forEach(f => {
-    // requiring the file.
-    // const file = require(`./dashboard/public/${f}`);
-    // if the file exists and has a name, do the following:
-    // if(file && file.name) {
-        // set "name" from inside the file as a route, and run the function.
-        // app.get(file.name, file.run);
+files.forEach(f => {
+    const file = require(`./dashboard/public/${f}`);
+    if(file && file.name) {
+        app.get(file.name, file.run);
         
-        // if(file.run2) app.post(file.name, file.run2);
+        if(file.run2) app.post(file.name, file.run2);
 
-        // Logs which files are being loaded.
-        //console.log(`[Dashboard] - Loaded ${file.name}`);
-    //}
-//});
+        console.log(`[Dashboard] - Loaded ${file.name}`);
+    }
+});
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+//const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-// for guild-based commands
 //rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: [] })
 	//.then(() => console.log('Successfully deleted all guild commands.'))
 	//.catch(console.error);
 
-// for global commands
 //rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: [] })
 	//.then(() => console.log('Successfully deleted all application commands.'))
 	//.catch(console.error);
@@ -80,7 +69,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
         console.log("Connected to DB.");
         eventHandler(client);
         client.login(process.env.TOKEN);
-        // app.listen(80, () => console.log("App on port 80"));
+        app.listen(80, () => console.log("App on port 80"));
     } catch (error) {
         console.log(`Error: ${error}`);
     }
