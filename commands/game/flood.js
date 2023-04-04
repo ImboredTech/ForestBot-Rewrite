a30f83ac82eb2d9400ad220d8514ba3624141098
ImboredTech <106279751+ImboredTech@users.noreply.github.com>
2023-04-04 13:30:48 +0100

    Create flood.js
    Created an new game command called flood js, still using the discord-gamecord library, this one though takes an argument called difficulty so you can change the difficulty of the game.

---
+++ commands/game/flood.js
@@ -0,0 +1,52 @@
+const { Flood } = require("discord-gamecord");
+
+const {
+    Client,
+    Interaction,
+    ApplicationCommandOptionType,
+    AttachmentBuilder,
+} = require("discord.js");
+
+module.exports = {
+    name: 'snake',
+    description: "Start an snake game.",
+    options: [
+        {
+            name: "difficulty",
+            description: "The difficulty you want to play at. (easy, normal, hard)",
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        },
+    ],
+    callback: async(client, interaction) => {
+        diff = interaction.options.get('difficulty');
+        diff.toLowerCase();
+        if(diff === "easy") {
+            difficulty = 8;
+        } else if(diff === "normal") {
+            difficulty = 13;
+        } else if(diff === "hard") {
+            difficulty = 18;
+        }
+        const Game = new Flood({
+            message: interaction,
+            isSlashGame: false,
+            embed: {
+                title: 'Flood',
+                color: '#5865F2',
+            },
+            difficulty: difficulty,
+            timeoutTime: 60000,
+            buttonStyle: "PRIMARY",
+            emojis: ['🟥', '🟦', '🟧', '🟪', '🟩'],
+            winMessage: 'You won! You took **{turns}** turns.',
+            loseMessage: 'You lost! You took **{turns}** turns.',
+            playerOnlyMessage: 'Only {player} can use these buttons.'
+        });
+
+        Game.startGame();
+        Game.on('gameOver', result => {
+            console.log(result);
+        });
+    }
+};
